50d120295e1de626d60879ae9b2e607ae61e7610
Jan200101 <sentrycraft123@gmail.com>
2024-05-27 11:55:29 +0200

    initial commit

---
+++ build.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) !void {
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+
+    // We use env vars throughout the build so we grab them immediately here.
+    var env = try std.process.getEnvMap(b.allocator);
+    defer env.deinit();
+
+    // We only build an exe if we have a runtime set.
+    const exe = b.addExecutable(.{
+        .name = "ghostty",
+        .root_source_file = b.path("empty.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const freetype_dep = b.dependency("freetype", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.root_module.addImport("freetype", freetype_dep.module("freetype"));
+    exe.linkLibrary(freetype_dep.artifact("freetype"));
+
+    b.installArtifact(exe);
+}
